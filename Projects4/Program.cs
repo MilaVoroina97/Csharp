b37570f605293f36121e1fe055963432ef4fcb28
mila <milavoronina1997@mail.ru>
2022-08-23 16:28:32 +0500

    2

--- Projects4/Program.cs
+++ Projects4/Program.cs
@@ -1,3 +1,6 @@
 ﻿double numberA = 12; // double позволяет выводит дробные числа или числа, которые не делятся нацело на число
 double numberB = 5;
 Console.WriteLine(numberA / numberB);
+
+
+new Random().Next(min,max) //генератор рандомных чисел 
