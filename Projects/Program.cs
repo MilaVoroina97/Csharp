d108ffa8b8c44ac670a13e7afe6ecb01389405a0
mila <milavoronina1997@mail.ru>
2022-08-23 16:28:16 +0500

    1

--- Projects/Program.cs
+++ Projects/Program.cs
@@ -1,4 +1,10 @@
-﻿Console.WriteLine("Введите Ваше имя:");
-string username = Console.ReadLine(); 
+﻿// ввести имя юзера и поприветствовать его
+Console.WriteLine("Введите Ваше имя:");
+string username = Console.ReadLine(); //команда считывания данных, вначале определяем тип данных 
 Console.WriteLine("Привет,");
 Console.WriteLine(username);
+
+
+
+//программа сложения двух чисел
+
