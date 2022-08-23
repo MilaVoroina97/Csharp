64635034e9538e42a15f72cbe263d9e7958821cd
mila <milavoronina1997@mail.ru>
2022-08-23 16:09:23 +0500

    Projects4/

--- Projects4/Program.cs
+++ Projects4/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double numberA = 12; // double позволяет выводит дробные числа или числа, которые не делятся нацело на число
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
